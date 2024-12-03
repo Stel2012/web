de5baabba27645c66c008ca20be2d30a85011213
Stel2012 <stel201@mail.ru>
2024-12-03 19:41:53 +0300

    The site is ready

--- db.php
+++ db.php
@@ -1,8 +1,50 @@
 <?php
-$link = mysqli_connect('127.0.0.1','root','123456');
+$server = "127.0.0.1";
+$user = "root";
+$pass = "123456";
+$dbName = "web";
+
+$link = mysqli_connect($server, $user, $pass);
+
 if (!$link){
-        die('Error:' . mysqli_error());
+        die('Ошибка подключения:' . mysqli_connect_error());
 }
-echo 'GOOD!';
+
+$sql = "CREATE DATABASE IF NOT EXISTS $dbName";
+
+if (!mysqli_query($link, $sql)){
+        echo "Не удалось создать БД";
+}
+
+mysqli_close($link);
+
+
+
+$link = mysqli_connect($server, $user, $pass, $dbName);
+
+$sql = "CREATE TABLE IF NOT EXISTS users(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(50) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        pass VARCHAR(50) NOT NULL
+)";
+
+
+if (!mysqli_query($link, $sql)){
+        echo "Не удалось создать таблицу Users";
+}
+
+
+
+$sql = "CREATE TABLE IF NOT EXISTS posts(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(20) NOT NULL,
+        main_text VARCHAR(400) NOT NULL
+)";
+
+
+if (!mysqli_query($link, $sql)){
+        echo "Не удалось создать таблицу Ports";
+}
 mysqli_close($link);
 ?>
